149f2aa7d6dbbf312476ccbcf4da66babac3f3a2
KBC12006149\futon <futo-yamauti-20h@stu.kbc.ac.jp>
2021-10-27 14:45:53 +0900

    moveground2スクリプトを新たに作成。これによりy軸が動く問題を解決。

---
+++ Assets/Script/MoveGround2.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround2 : MonoBehaviour
+{
+    float x;
+    float z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左右キーの入力を取得
+        float horizontal = Input.GetAxis("Horizontal")*-1;
+        //上下キーの入力を取得
+        float vertical = Input.GetAxis("Vertical");
+
+        // 現在のGameObjectのZ軸方向の角度を取得
+        float currentZAngle = transform.eulerAngles.z;
+        //現在のGameObjectのX軸方向の角度を取得
+        float currentXAngle = transform.eulerAngles.x;
+
+        //現在の角度が180より大きい場合
+        if (currentXAngle > 180)
+        {
+            // デフォルトでは角度は0～360なので-180～180となるように補正
+            currentXAngle = currentXAngle - 360;
+        }
+
+        //X軸の回転処理(上ボタン）
+        if (vertical > 0)
+        {
+            if (currentXAngle < 30)
+            {
+                x += Time.deltaTime * 20;
+                
+            }
+            else
+            {
+                x = 30;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+        //上ボタンがが押されていないかつ、現在のX軸の角度が0より大きい時に傾きを0度に戻す処理
+        if (vertical == 0&&currentXAngle>0)
+        {
+            if (currentXAngle > 0.1)
+            {
+                x += Time.deltaTime * -30;
+            }
+            else
+            {
+                x = 0;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+
+        //X軸の回転処理(下ボタン)
+        if (vertical < 0)
+        {
+            if (currentXAngle > -30)
+            {
+                x += Time.deltaTime * -20;
+
+            }
+            else
+            {
+                x = -30;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+        if (vertical == 0&&currentXAngle<0)
+        {
+            if (currentXAngle < -0.1)
+            {
+                x += Time.deltaTime * 30;
+            }
+            else
+            {
+                x = 0;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+        //現在の角度が180より大きい場合
+        if (currentZAngle > 180)
+        {
+            // デフォルトでは角度は0～360なので-180～180となるように補正
+            currentZAngle = currentZAngle - 360;
+        }
+
+        //Z軸の回転処理(左ボタン）
+        if (horizontal > 0)
+        {
+            if (currentZAngle < 30)
+            {
+                z += Time.deltaTime * 20;
+
+            }
+            else
+            {
+                z = 30;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+        if (horizontal == 0 && currentZAngle > 0)
+        {
+            if (currentZAngle > 0.1)
+            {
+                z += Time.deltaTime * -30;
+            }
+            else
+            {
+                z = 0;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+        //Z軸の回転処理(右ボタン）
+        if (horizontal < 0)
+        {
+            if (currentZAngle > -30)
+            {
+                z += Time.deltaTime * -20;
+
+            }
+            else
+            {
+                z = -30;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+        if (horizontal == 0 && currentZAngle < 0)
+        {
+            if (currentZAngle < -0.1)
+            {
+                z += Time.deltaTime * 30;
+            }
+            else
+            {
+                z = 0;
+            }
+            transform.rotation = Quaternion.Euler(x, 0, z);
+        }
+
+    }
+}
