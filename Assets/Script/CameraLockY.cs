2d1264396a760df848d941160bd62245048ff594
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-10-28 15:06:34 +0900

    カメラ　15Cmの縛りはあきらめた

--- Assets/Script/CameraLockY.cs
+++ Assets/Script/CameraLockY.cs
@@ -9,12 +9,7 @@ public class CameraLockY : CinemachineExtension
     [Tooltip("カメラのY座標を固定する値")]
     public float m_YPosition = 10;
 
-    [Tooltip("カメラの回転を固定する値")]
-    public float m_RotePosition;
-
-    protected override void PostPipelineStageCallback(
-        CinemachineVirtualCameraBase vcam,
-        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    protected override void PostPipelineStageCallback( CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
         if(stage == CinemachineCore.Stage.Body)
         {
@@ -22,10 +17,5 @@ public class CameraLockY : CinemachineExtension
             pos.y = m_YPosition;
             state.RawPosition = pos;
         }
-    }
-
-    void Update()
-    {
-        this.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
