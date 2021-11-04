6127f05f8b27bb4c0a8f632ee75596371f2bbdee
shotakinjou <hakumaimk-777>
2021-11-04 15:46:20 +0900

    ・コインにタグ[Coin]追加　・取得したコインを数えるscript「Count」を追加(未完成）・一部UIの位置を調整

---
+++ Assets/Script/Count.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour
+{
+    public Text countText;
+    private int count;
+    private int CoinNum;
+    void Start()
+    {
+        count = 0;
+        CoinNum = GameObject.Find("Coin").transform.childCount;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+}
