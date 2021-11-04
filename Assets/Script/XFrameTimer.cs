685b2ab6afe3e162426764adb930623853cb1508
KBC12006149\futon <futo-yamauti-20h@stu.kbc.ac.jp>
2021-11-04 13:59:23 +0900

    床の傾き時間を測定するフレームタイマースクリプトの完成　

--- Assets/Script/XFrameTimer.cs
+++ Assets/Script/XFrameTimer.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FrameTimer : MonoBehaviour
+public class XFrameTimer : MonoBehaviour
 {
     float countTime = 0;
+
+    float oldAngle = 0;
 
     [SerializeField]
     GameObject ground;
@@ -21,15 +23,26 @@ public class FrameTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mv2.currentXAngle < 30)
+
+        if (oldAngle == 0 && mv2.currentXAngle != 0)
+        {
+            countTime = 0;
+            GetComponent<Text>().text = countTime.ToString("F2");
+        }
+        if (mv2.currentXAngle > -30 && mv2.currentXAngle < 30 && mv2.currentXAngle != 0)
         {
             // countTimeに、ゲームが開始してからの秒数を格納
             countTime += Time.deltaTime;
 
             // 小数2桁にして表示
             GetComponent<Text>().text = countTime.ToString("F2");
+
+            if (mv2.vertical == 0)
+            {
+                countTime -= Time.deltaTime * 2;
+                GetComponent<Text>().text = countTime.ToString("F2");
+            }
         }
-
-
+        oldAngle = mv2.currentXAngle;
     }
 }
