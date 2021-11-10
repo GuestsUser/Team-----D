d4129b80d2a2278390a5e9602c491e1aec995db0
yoshi <d>
2021-11-10 10:22:55 +0900

    リザルト画面追加したけどまだボタンが不安定

--- Assets/Script/clear.cs
+++ Assets/Script/clear.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI; // 追加
+using UnityEngine.SceneManagement;
 
 class clear : MonoBehaviour
 {
@@ -9,31 +10,37 @@ class clear : MonoBehaviour
     public Text gameClearMessage; // 追加
     Transform myTransform;
     bool clearsound = false;
+    private float step_time = 0;
+
     void Start()
     {
         audioSource = this.gameObject.GetComponent<AudioSource>();
         myTransform = transform;
-      
+
     }
 
     void Update()
     {
+        step_time += Time.deltaTime;
+
         if (!clearsound)
             if (myTransform.childCount == 0)
-           
+
             {
-                
+
                 gameClearMessage.text = "Game Clear"; // 追加
-                     
-               
+
+
                 audioSource.Play();
                 clearsound = true;
-               
+
                 Time.timeScale = 0f;
                 clearUI.SetActive(false);
-
+                
+                if (step_time >= 17.0f)
+                {
+                    SceneManager.LoadScene("result");
+                }
             }
     }
-
-    
 }
