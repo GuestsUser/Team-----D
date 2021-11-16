b3a86f71b350b190636338e26da272af11fc2cb4
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-11-16 15:02:30 +0900

    エフェクト処理　いろいろやってたけど進捗無し

---
+++ Assets/Script/Particle_Sparks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_Sparks : MonoBehaviour
+{
+    public ParticleSystem spark;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            spark.transform.position = other.transform.position;
+            spark.Play();
+        }
+    }
+}
